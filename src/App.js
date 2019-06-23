ce197888a308fea2d1585467d5b4131c17eb0d85
Ximena Cordero <cor.xime@gmail.com>
2019-06-23 00:44:00 -0400

    Third commit
    React has been installed instead of being called by the CDK

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-const Pet = ({ name, animal, color }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h3", {}, color)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "sometihng-important" }, "Adopt Me!!!"),
-    React.createElement(Pet, {
-      name: "Agnes",
-      animal: "Rabbit",
-      color: "gold"
-    }),
-    React.createElement(Pet, {
-      name: "Dante",
-      animal: "Rabbit",
-      color: "White and Black"
-    }),
-    React.createElement(Pet, {
-      name: "Negrita",
-      animal: "Rabbit",
-      color: "Dark"
-    })
-  ]);
+    return React.createElement("div", {}, [
+        React.createElement("h1", { id: "sometihng-important" }, "Adopt Me!!!"),
+        React.createElement(Pet, {
+            name: "Agnes",
+            animal: "Rabbit",
+            color: "gold"
+        }),
+        React.createElement(Pet, {
+            name: "Dante",
+            animal: "Rabbit",
+            color: "White and Black"
+        }),
+        React.createElement(Pet, {
+            name: "Negrita",
+            animal: "Rabbit",
+            color: "Dark"
+        })
+    ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
